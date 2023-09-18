1811bd8b79ba630eed3bbcf83cdb1e904e5c5a14
liubo0902 <38622806+liubo0902@users.noreply.github.com>
2023-09-18 10:14:26 +0800

    Update m2m_config.py

--- scripts/m2m_config.py
+++ scripts/m2m_config.py
@@ -1,2 +1,4 @@
-mov2mov_outpath_samples = 'outputs/mov2mov-images'
-mov2mov_output_dir = 'outputs/mov2mov-videos'
+from modules.shared import cmd_opts
+
+mov2mov_outpath_samples = f'{cmd_opts.data_dir}/outputs/mov2mov-images'
+mov2mov_output_dir = f'{cmd_opts.data_dir}/outputs/mov2mov-videos'
