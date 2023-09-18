251912cf064e3631214220be0bdc6369e94787ad
liubo0902 <38622806+liubo0902@users.noreply.github.com>
2023-09-18 14:05:09 +0800

    Update m2m_modnet.py

--- scripts/m2m_modnet.py
+++ scripts/m2m_modnet.py
@@ -13,6 +13,7 @@ import torchvision.transforms as transforms
 import modules.paths as ph
 from scripts.modnet.modnet import MODNet
 from modules.shared import cmd_opts
+import hashlib
 
 """
 from modnet-entry("https://github.com/RimoChan/modnet-entry")
@@ -21,6 +22,49 @@ modnet_models_path = f'{cmd_opts.data_dir}/models/mov2mov-ModNet'
 if os.environ.get('SERVICE_NAME', '') != '' and cmd_opts.just_ui:
     modnet_models_path = f'{os.path.dirname(cmd_opts.data_dir)}/models/mov2mov-ModNet'
 os.makedirs(modnet_models_path, exist_ok=True)
+
+modnet_photographic_path = f"{modnet_models_path}/modnet_photographic_portrait_matting.ckpt"
+modnet_webcam_path = f"{modnet_models_path}/modnet_webcam_portrait_matting.ckpt"
+
+modnet_photographic_url = "https://huggingface.co/DavG25/modnet-pretrained-models/resolve/main/models/modnet_photographic_portrait_matting.ckpt"
+modnet_photographic_checksum = "db1f7ec96b370abebbd506e360ce9819380cb45bb99930f1e955dfcbe9e4035708a6190ebf292e0b8b740cefdc879bad9146ba8158baac0a702aadf5311a8cd6"
+
+modnet_webcam_url = "https://huggingface.co/DavG25/modnet-pretrained-models/resolve/main/models/modnet_webcam_portrait_matting.ckpt"
+modnet_webcam_checksum = "19ec6baa9934f834739d496c6f5d119a4e6fe09f67d19342025d3e9199b694814bc89d4e866db1d11da5e1740eba72a2ff4116420c5c212bf886d330d526f603"
+
+# Compare checksum to make sure downloaded file is not modified
+def checksum(filename, hash_factory=hashlib.blake2b, chunk_num_blocks=128):
+    h = hash_factory()
+    with open(filename,'rb') as f: 
+        while chunk := f.read(chunk_num_blocks*h.block_size): 
+            h.update(chunk)
+    return h.hexdigest()
+
+# Download modnet_photographic_portrait_matting.ckpt
+if not os.path.exists(modnet_photographic_path):
+    print('Downloading model for mov2mov ModNet, this is a one time operation')
+    from basicsr.utils.download_util import load_file_from_url
+    load_file_from_url(modnet_photographic_url, model_dir=modnet_models_path)
+
+    if checksum(modnet_photographic_path) != modnet_photographic_checksum:
+        os.remove(modnet_photographic_path)
+        print(f"\nWarning: unable to automatically downloading ModNet model for mov2mov (checksum mismatch), please manually download from {modnet_photographic_url} and place in the folder {modnet_models_path}\n")
+    else:
+        print('Model download for mov2mov ModNet completed\n')
+
+# Download modnet_webcam_portrait_matting.ckpt
+if not os.path.exists(modnet_webcam_path):
+    print('Downloading model for mov2mov ModNet, this is a one time operation')
+    from basicsr.utils.download_util import load_file_from_url
+    load_file_from_url(modnet_webcam_url, model_dir=modnet_models_path)
+
+    if checksum(modnet_webcam_path) != modnet_webcam_checksum:
+        os.remove(modnet_webcam_path)
+        print(f"\nWarning: unable to automatically downloading ModNet model for mov2mov (checksum mismatch), please manually download from {modnet_webcam_url} and place in the folder {modnet_models_path}\n")
+    else:
+        print('Model download for mov2mov ModNet completed\n')
+
+
 modnet_models = ['none'] + [model for model in os.listdir(modnet_models_path) if model.endswith('.ckpt')]
 
 
